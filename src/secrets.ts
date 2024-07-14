8ef01bc0658d25e506d522b221bab1004c08ac13
Jonathan Martinez <Jmartinez070589@gmail.com>
2024-07-13 20:49:34 -0500

    Initial commit initializing the reskipper app with vite and react.

---
+++ src/secrets.ts
@@ -0,0 +1,9 @@
+export const spreadsheetId = '1Hvfj0p9qhtWidUAft46gnjaLi0GPufP_LefuDVrcozk';
+
+export const QUERY_STRING =
+  'https://agent.onehome.com/en-US/properties/map?token=eyJPU04iOiJOVFIiLCJ0eXBlIjoiMSIsInNldGlkIjoiNTU4NTY4MiIsInNldGtleSI6IjMyMiIsImVtYWlsIjoiam1hcnRpbmV6MDcwNTg5QGdtYWlsLmNvbSIsInJlc291cmNlaWQiOjAsImFnZW50aWQiOjE2NDE4OSwiaXNkZWx0YSI6ZmFsc2UsIlZpZXdNb2RlIjoiMiIsInNvdXJjZSI6Ik1hdHJpeFVJIn0%3D&SMS=0&searchId=dfc986bf-5553-3782-8ffd-05397474f09f' as const;
+
+export const allowedUsers = [
+  { username: 'TreyAkerRescraper', password: 'TaReScrape!2q?' },
+  { username: 'JonathanMartinezRescraper', password: 'JmReScrape?5x!' },
+];
