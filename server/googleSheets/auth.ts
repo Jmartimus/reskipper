d83f12d5cefe236656aa8e6b613d446646112b29
Jonathan Martinez <Jmartinez070589@gmail.com>
2024-08-14 05:36:16 -0500

    Moved reskipper-key file

--- server/googleSheets/auth.ts
+++ server/googleSheets/auth.ts
@@ -5,7 +5,7 @@ import path, { dirname } from 'path';
 
 export const authenticateSheets = async (): Promise<Sheets> => {
   const dirName = dirname(__filename);
-  const keyFilePath = path.join(dirName, '../../src/assets/reskipper-key.json');
+  const keyFilePath = path.join(dirName, '../../public/reskipper-key.json');
   const auth = new GoogleAuth({
     keyFile: keyFilePath,
     scopes: 'https://www.googleapis.com/auth/spreadsheets',
